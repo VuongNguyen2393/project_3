aeab77a99280ef96a1925adbc2100b70c2add95d
VuongND9 <nguyendinhvuong.act@gmail.com>
2022-12-03 22:50:51 +0700

    firt commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=vuongnd9
+export POSTGRES_PASSWORD=vuongnd9
+export POSTGRES_HOST=database-project-3.cjgw9wpaz8no.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-263304476420-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
