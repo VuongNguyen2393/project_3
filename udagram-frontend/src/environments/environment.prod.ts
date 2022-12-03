70241b7e6f762864959976f8a43c5dc90a726bb7
VuongND9 <nguyendinhvuong.act@gmail.com>
2022-12-04 03:14:54 +0700

    update frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a751014eed25b4ddd8ab1d9ea7d3aa08-1255475112.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
