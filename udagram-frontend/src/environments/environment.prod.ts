8cdf191ac8fe7d79ecf859cb48285266d0260ffb
VuongND9 <nguyendinhvuong.act@gmail.com>
2022-12-06 20:36:32 +0700

    update kubectl describe hpa

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aec8db473a77841889d9dabda33c6b6a-117425892.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
